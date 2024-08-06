ac6f4379bc86a0c88d972fcf0f71ae19376fe63a
Sarah Löffler <sarah.loeffler@hmmh.de>
2024-08-06 13:41:49 +0200

    colors

---
+++ lib/colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class GlobalThemData {
+  static ThemeData lightThemeData =
+      themeData(lightColorScheme, Color(0xFF8C1A6A));
+
+  static ThemeData darkThemeData =
+      themeData(darkColorScheme, Color(0xFF8C1A6A));
+
+  static ThemeData themeData(ColorScheme colorScheme, Color focusColor) {
+    return ThemeData(
+        colorScheme: colorScheme,
+        canvasColor: colorScheme.surface,
+        scaffoldBackgroundColor: colorScheme.surface,
+        highlightColor: Colors.transparent,
+        focusColor: focusColor);
+  }
+
+  static ColorScheme lightColorScheme = ColorScheme(
+    primary: Color(0xFFEFC7E5),
+    onPrimary: Color(0xFF111D13),
+    secondary: Color(0xFF8C1A6A),
+    onSecondary: Color(0xFFFAFAFA),
+    error: Color(0xFFB90E0A),
+    onError: Color(0xFFFAFAFA),
+    surface: Color(0xFFFAFAFA),
+    onSurface: Color(0xFF111D13),
+    brightness: Brightness.light,
+  );
+
+  static ColorScheme darkColorScheme = ColorScheme(
+    primary: Color(0xFFEFC7E5),
+    onPrimary: Color(0xFF111D13),
+    secondary: Color(0xFF8C1A6A),
+    onSecondary: Color(0xFFFAFAFA),
+    error: Color(0xFFB90E0A),
+    onError: Color(0xFFFAFAFA),
+    surface: Color(0xFF111D13),
+    onSurface: Color(0xFFFAFAFA),
+    brightness: Brightness.dark,
+  );
+}
