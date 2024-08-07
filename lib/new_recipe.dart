f5b2d0806cef89952b1925f513ddf685ef1dce7a
Sarah Löffler <sarah.loeffler@hmmh.de>
2024-08-07 11:38:38 +0200

    title + first ingredients textfields

--- lib/new_recipe.dart
+++ lib/new_recipe.dart
@@ -8,18 +8,76 @@ class NewRecipe extends StatefulWidget {
 }
 
 class _NewRecipeState extends State<NewRecipe> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Column(
-        children: [
-          Container(width: double.infinity, height: 50, child: Text("hi")),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Test"))
-        ],
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+          child: Column(
+            children: [
+              TextFormField(
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Titel',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Füge einen Titel hinzu';
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.sizeOf(context).width / 7,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Menge',
+                        hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.sizeOf(context).width / 7,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Einheit',
+                        hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.sizeOf(context).width / 2.25,
+                    child: TextFormField(
+                      onFieldSubmitted: (value) {
+                        print("submitted");
+                      },
+                      decoration: InputDecoration(
+                          hintText: 'Zutat',
+                          hintStyle: TextStyle(fontWeight: FontWeight.w300)),
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    //daten speichern
+                  }
+                },
+                child: const Text('Speichern'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
