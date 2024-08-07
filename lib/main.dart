bf1def50154069ae5c17fd48b6dd40bf28aa99a4
Sarah Löffler <sarah.loeffler@hmmh.de>
2024-08-07 09:18:45 +0200

    categories buttons

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List catgories = [
+    {"name": "Salate", "imageName": "salad.jpeg"},
+    {"name": "Hauptgerichte", "imageName": "hauptgerichte.jpg"},
+    {"name": "Brote", "imageName": "brote.jpg"},
+    {"name": "Süßspeisen", "imageName": "sweets.jpg"},
+    {"name": "Getränke", "imageName": "drinks.jpg"},
+    {"name": "Sonstiges", "imageName": "sonstiges.jpg"},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -52,165 +53,65 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 30, left: 20, right: 20),
-              width: double.infinity,
-              height: 140,
-              child: Material(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: InkWell(
-                  splashColor: Theme.of(context).colorScheme.primary,
-                  onTap: () {},
-                  child: Stack(
-                    children: [
-                      Ink.image(
-                        image: AssetImage('assets/salad.jpeg'),
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        color: Color(0xCFFFFFFF),
-                      ),
-                      Center(
-                        child: Text(
-                          "Salate",
-                          style: GoogleFonts.manrope(
-                              fontSize: 40,
-                              fontWeight: FontWeight.w800,
-                              color: Theme.of(context).colorScheme.onPrimary),
+            for (var category in catgories)
+              Container(
+                padding: EdgeInsets.only(top: 30, left: 20, right: 20),
+                width: double.infinity,
+                height: 160,
+                child: Material(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: InkWell(
+                    splashColor: Theme.of(context).colorScheme.primary,
+                    onTap: () {},
+                    child: Stack(
+                      children: [
+                        Ink.image(
+                          image: AssetImage('assets/${category["imageName"]}'),
+                          fit: BoxFit.cover,
                         ),
-                      )
-                    ],
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  padding: EdgeInsets.all(10),
+                                  child: Text(
+                                    category["name"],
+                                    style: GoogleFonts.manrope(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w800,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary),
+                                  ),
+                                )
+                              ]),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 30, left: 20, right: 20),
-              width: double.infinity,
-              height: 140,
-              child: Material(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: InkWell(
-                  splashColor: Theme.of(context).colorScheme.primary,
-                  onTap: () {},
-                  child: Stack(
-                    children: [
-                      Ink.image(
-                        image: AssetImage('assets/salad.jpeg'),
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        color: Color(0xCFFFFFFF),
-                      ),
-                      Center(
-                        child: Text(
-                          "Salate",
-                          style: GoogleFonts.manrope(
-                              fontSize: 40,
-                              fontWeight: FontWeight.w800,
-                              color: Theme.of(context).colorScheme.onPrimary),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 30, left: 20, right: 20),
-              width: double.infinity,
-              height: 140,
-              child: Material(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: InkWell(
-                  splashColor: Theme.of(context).colorScheme.primary,
-                  onTap: () {},
-                  child: Stack(
-                    children: [
-                      Ink.image(
-                        image: AssetImage('assets/salad.jpeg'),
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        color: Color(0xCFFFFFFF),
-                      ),
-                      Center(
-                        child: Text(
-                          "Salate",
-                          style: GoogleFonts.manrope(
-                              fontSize: 40,
-                              fontWeight: FontWeight.w800,
-                              color: Theme.of(context).colorScheme.onPrimary),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 30, left: 20, right: 20),
-              width: double.infinity,
-              height: 140,
-              child: Material(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: InkWell(
-                  splashColor: Theme.of(context).colorScheme.primary,
-                  onTap: () {},
-                  child: Stack(
-                    children: [
-                      Ink.image(
-                        image: AssetImage('assets/salad.jpeg'),
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        color: Color(0xCFFFFFFF),
-                      ),
-                      Center(
-                        child: Text(
-                          "Salate",
-                          style: GoogleFonts.manrope(
-                              fontSize: 40,
-                              fontWeight: FontWeight.w800,
-                              color: Theme.of(context).colorScheme.onPrimary),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            SizedBox(
+              height: 90,
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {},
+        tooltip: 'Neues Rezept',
+        child: Icon(Icons.add),
       ),
     );
   }
