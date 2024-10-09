69a28f635b54a6f59c15e13e7d0a5322e1584d99
sarahLoeffler <sarah.loeffler42@gmail.com>
2024-10-09 15:12:51 +0200

    reminder dialog on back press

--- lib/new_recipe.dart
+++ lib/new_recipe.dart
@@ -129,188 +129,226 @@ class _NewRecipeState extends State<NewRecipe> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Form(
-        key: _formKey,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-          child: Column(
-            children: [
-              Text(
-                "Neues Rezept in ${widget.category}",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
-              ),
-              SizedBox(height: 30),
-              TextFormField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                controller: titleController,
-                style: GoogleFonts.indieFlower(fontSize: 30),
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Titel',
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) async {
+        if (didPop) {
+          return;
+        }
+        if (titleController.value.text.isNotEmpty ||
+            image != null ||
+            (nameTECs.keys.isNotEmpty && nameTECs[0]!.value.text.isNotEmpty) ||
+            instructionsController.value.text.isNotEmpty) {
+          showDialog(
+            useRootNavigator: false,
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                content: Text("Willst du alle Eingaben löschen?"),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Ja, alles löschen")),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Abbrechen")),
+                ],
+              );
+            },
+            barrierDismissible: false,
+          );
+        } else {
+          Navigator.pop(context);
+        }
+      },
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+            child: Column(
+              children: [
+                Text(
+                  "Neues Rezept in ${widget.category}",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Füge einen Titel hinzu';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 15),
-              Column(children: [
-                GestureDetector(
-                  onTap: () async {
-                    final XFile? pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
-                    final pathVar = pickedImage?.path;
-                    if (pathVar == null) {
-                      return;
+                SizedBox(height: 30),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  controller: titleController,
+                  style: GoogleFonts.indieFlower(fontSize: 30),
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Titel',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Füge einen Titel hinzu';
                     }
-                    File file = File(pathVar);
-                    setState(() {
-                      image = file;
-                    });
+                    return null;
                   },
-                  child: (image != null)
-                      ? ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.file(
-                            image!,
-                            fit: BoxFit.cover,
-                          ))
-                      : Container(
-                          width: MediaQuery.sizeOf(context).width * 0.5,
-                          height: MediaQuery.sizeOf(context).width * 0.3,
-                          decoration: ShapeDecoration(
-                              color: Colors.black12,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
-                          child: Icon(
-                            Icons.photo,
-                            size: MediaQuery.sizeOf(context).width * 0.15,
-                            color: Colors.grey,
-                          ),
-                        ),
                 ),
-              ]),
-              SizedBox(height: 30),
-              Card(
-                elevation: 3,
-                color: Theme.of(context).colorScheme.surfaceDim,
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Zutaten",
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 10),
-                      ListView.builder(
-                          shrinkWrap: true,
-                          physics: ScrollPhysics(),
-                          itemCount: item.length,
-                          itemBuilder: (context, index) {
-                            return item.values.elementAt(index);
-                          }),
-                    ],
+                SizedBox(height: 15),
+                Column(children: [
+                  GestureDetector(
+                    onTap: () async {
+                      final XFile? pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+                      final pathVar = pickedImage?.path;
+                      if (pathVar == null) {
+                        return;
+                      }
+                      File file = File(pathVar);
+                      setState(() {
+                        image = file;
+                      });
+                    },
+                    child: (image != null)
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.file(
+                              image!,
+                              fit: BoxFit.cover,
+                            ))
+                        : Container(
+                            width: MediaQuery.sizeOf(context).width * 0.5,
+                            height: MediaQuery.sizeOf(context).width * 0.3,
+                            decoration: ShapeDecoration(
+                                color: Colors.black12,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                            child: Icon(
+                              Icons.photo,
+                              size: MediaQuery.sizeOf(context).width * 0.15,
+                              color: Colors.grey,
+                            ),
+                          ),
+                  ),
+                ]),
+                SizedBox(height: 30),
+                Card(
+                  elevation: 3,
+                  color: Theme.of(context).colorScheme.surfaceDim,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Zutaten",
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        ListView.builder(
+                            shrinkWrap: true,
+                            physics: ScrollPhysics(),
+                            itemCount: item.length,
+                            itemBuilder: (context, index) {
+                              return item.values.elementAt(index);
+                            }),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              Card(
-                elevation: 3,
-                color: Theme.of(context).colorScheme.surfaceDim,
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(children: [
-                    Text(
-                      "Zubereitung",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      controller: instructionsController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        border: OutlineInputBorder(),
+                SizedBox(height: 30),
+                Card(
+                  elevation: 3,
+                  color: Theme.of(context).colorScheme.surfaceDim,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(children: [
+                      Text(
+                        "Zubereitung",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                      maxLines: 15,
-                    ),
-                  ]),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        controller: instructionsController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          border: OutlineInputBorder(),
+                        ),
+                        maxLines: 15,
+                      ),
+                    ]),
+                  ),
                 ),
-              ),
-              SizedBox(height: 30),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.secondary,
-                    textStyle: TextStyle(color: Theme.of(context).colorScheme.onSecondary)),
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    ingredients.clear();
+                SizedBox(height: 30),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      textStyle: TextStyle(color: Theme.of(context).colorScheme.onSecondary)),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      ingredients.clear();
 
-                    for (int i = 0; i <= nameTECs.keys.last; i++) {
-                      var amount = amountTECs[i]?.value.text;
-                      var unit = unitTECs[i]?.value.text;
-                      var name = nameTECs[i]?.value.text;
+                      for (int i = 0; i <= nameTECs.keys.last; i++) {
+                        var amount = amountTECs[i]?.value.text;
+                        var unit = unitTECs[i]?.value.text;
+                        var name = nameTECs[i]?.value.text;
 
-                      if (name != null && name != "") {
-                        ingredients.add(Ingredient(amount, unit, name));
+                        if (name != null && name != "") {
+                          ingredients.add(Ingredient(amount, unit, name));
+                        }
                       }
-                    }
 
-                    Recipe recipe = Recipe(titleController.value.text, image, widget.category, ingredients,
-                        instructionsController.value.text);
-                    showDialog(
-                      useRootNavigator: false,
-                      context: context,
-                      builder: (_) {
-                        return AlertDialog(
-                          content: Row(
-                            children: [
-                              CircularProgressIndicator(color: Theme.of(context).primaryColor),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              const Text("Loading ..."),
-                            ],
-                          ),
-                        );
-                      },
-                      barrierDismissible: false,
-                    );
-                    var result = await MySQL().recipeEntry(recipe);
-                    setState(() {});
-                    if (context.mounted) {
-                      Navigator.pop(context);
-                      if (result != true) {
-                        showDialog(
-                          useRootNavigator: false,
-                          context: context,
-                          builder: (_) {
-                            return AlertDialog(
-                              title: Text("Fehlermeldung"),
-                              content: Text(result.toString()),
-                              actions: [
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text("Okay")),
+                      Recipe recipe = Recipe(titleController.value.text, image, widget.category, ingredients,
+                          instructionsController.value.text);
+                      showDialog(
+                        useRootNavigator: false,
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            content: Row(
+                              children: [
+                                CircularProgressIndicator(color: Theme.of(context).primaryColor),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                const Text("Loading ..."),
                               ],
-                            );
-                          },
-                          barrierDismissible: false,
-                        );
-                      } else {
+                            ),
+                          );
+                        },
+                        barrierDismissible: false,
+                      );
+                      var result = await MySQL().recipeEntry(recipe);
+                      setState(() {});
+                      if (context.mounted) {
                         Navigator.pop(context);
+                        if (result != true) {
+                          showDialog(
+                            useRootNavigator: false,
+                            context: context,
+                            builder: (_) {
+                              return AlertDialog(
+                                title: Text("Fehlermeldung"),
+                                content: Text(result.toString()),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("Okay")),
+                                ],
+                              );
+                            },
+                            barrierDismissible: false,
+                          );
+                        } else {
+                          Navigator.pop(context);
+                        }
                       }
                     }
-                  }
-                },
-                child: Text('Speichern'),
-              ),
-            ],
+                  },
+                  child: Text('Speichern'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
